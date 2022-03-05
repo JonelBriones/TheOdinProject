50a01173fe4bb4522fe33050da4c14bd805f1da6
Jonel Briones <jonel.c.briones@gmail.com>
2022-03-04 19:05:51 -0800

    temp conversion completed

--- TheOdinProjectJavaScript/07_tempConversion/tempConversion.js
+++ TheOdinProjectJavaScript/07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(ftoc) {
+  ftoc = (ftoc -32) * (5 / 9);
+  // rounds decimals by the 0.1
+  ftoc = Math.round(ftoc * 10) / 10;
+  return ftoc;
 };
 
-const ctof = function() {
-
+const ctof = function(ctof) {
+  ctof = (ctof * (9 / 5) + 32) ;
+  // rounds decimals by the 0.1
+  ctof = Math.round(ctof * 10) / 10;
+  return ctof;
 };
 
 // Do not edit below this line
